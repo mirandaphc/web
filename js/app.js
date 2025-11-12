bcbd03ef98e8998f6006004fe45ce4294888eaff
mirandaphc <perezhita.miranda@gmail.com>
2025-11-12 17:15:09 +0100

    contenido about

--- js/app.js
+++ js/app.js
@@ -195,7 +195,7 @@ function renderAbout() {
   body.className = "about-body";
 
   const h2 = document.createElement("h2");
-  h2.textContent = "Miranda Pérez-Hita";
+  h2.textContent = "";
   body.appendChild(h2);
 
   const paragraphs = getLocalizedParagraphs(aboutData?.text, activeLanguage);
